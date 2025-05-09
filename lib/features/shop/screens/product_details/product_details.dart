034bb0033ed716fb0733b6c656e1a57c8d391c69
rahmabenomrane <rahmabenomrane5@gmail.com>
2025-05-09 15:11:34 +0100

    prod details ui p2

--- lib/features/shop/screens/product_details/product_details.dart
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -1,5 +1,8 @@
 import 'package:examenmobile/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:examenmobile/common/widgets/images/t_rounded_image.dart';
+import 'package:examenmobile/common/widgets/texts/section_heading.dart';
+import 'package:examenmobile/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
+import 'package:examenmobile/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:examenmobile/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:examenmobile/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:examenmobile/features/shop/screens/product_details/widgets/rating_share_widget.dart';
@@ -10,6 +13,7 @@ import 'package:examenmobile/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
@@ -17,12 +21,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             // 1 - Product Image Slider
-            TProductImageSlider(),
+            const TProductImageSlider(),
 
             /// 2 - Product Details
             Padding(
@@ -34,10 +39,44 @@ class ProductDetailScreen extends StatelessWidget {
               child: Column(
                 children: [
                   /// - Rating & Share Button
-                  TRatingAndShare(),
-
-                  TProductMetaData(),
+                  const TRatingAndShare(),
                   //price,title
+                  const TProductMetaData(),
+
+                  //attributes
+                  const TProductAttributes(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+
+                  ///checkou button
+                  SizedBox(width: double.infinity,child: ElevatedButton(onPressed: () {}, child: const Text('Checkout'))),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  //description
+                  const TSectionHeading(title: 'Description',showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems ),
+                  ReadMoreText(
+                    'Keep your pet warm and stylish with this cozy animal coat. Made from soft, breathable fabric, it ensures comfort during chilly days. Easy to put on and perfect for outdoor walks or indoor lounging.',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: 'Show more',
+                    trimExpandedText: 'Show less',
+                    moreStyle: TextStyle(fontSize: 14,fontWeight: FontWeight.w800),
+                    lessStyle: TextStyle(fontSize: 14,fontWeight: FontWeight.w800),
+                  ),
+                  
+                  ///reviews
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  
+                  Row(
+                    children: [
+                      const TSectionHeading(title: 'Reviews(100)',showActionButton: false),
+
+                      IconButton(icon: const Icon(Iconsax.arrow_right_3,size: 18),onPressed: () {},)
+                    ],
+                  )
+
+
                 ],
               ),
             ),
